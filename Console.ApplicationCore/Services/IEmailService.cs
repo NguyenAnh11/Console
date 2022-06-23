cc1d25b9e3a41481f098b15cbe88f3c8f3a9bae4
dungpv <dungpv@novaon.vn>
2022-06-23 09:57:34 +0700

    add send email

---
+++ Console.ApplicationCore/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Console.ApplicationCore.Services
+{
+    public interface IEmailService
+    {
+        Task SendAsync(string to, string subject, string body);
+    }
+}
---
+++ Console.Module.Email/Configurations/EmailConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Console.Module.Email.Configurations
+{
+    public class EmailConfig
+    {
+        public string Provider { get; set; }
+        public SmtpConfig Smtp { get; set; }
+    }
+
+    public class SmtpConfig
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
